f617b7eb58b40eee91081eaea59ede88d388e63c
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-05-31 05:28:16 -0400

    Add menu toggle

--- class08/layout7-hair-salon/js/main.js
+++ class08/layout7-hair-salon/js/main.js
@@ -1,1 +1,15 @@
+const menuIcon = document.querySelector(".menu-icon")
+const closeIcon = document.querySelector(".close-icon")
+const navMenu = document.querySelector(".nav-menu")
 
+const toggleMenu = () => {
+  navMenu.classList.toggle("visually-hidden")
+  menuIcon.classList.toggle("visually-hidden")
+}
+
+document.addEventListener("click", (e) => {
+  const outsideClick = !navMenu.contains(e.target)
+  outsideClick && toggleMenu()
+})
+
+menuIcon.addEventListener("click", toggleMenu)
